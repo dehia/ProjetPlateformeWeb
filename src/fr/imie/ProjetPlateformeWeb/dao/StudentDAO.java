4ad30e43183e3ad22353326ffa8c292526f5945c
YoanneRand <yoanne.randria@gmail.com>
2017-01-11 14:46:03 +0100

    tester la fonctionnalité du projet

---
+++ src/fr/imie/ProjetPlateformeWeb/dao/StudentDAO.java
@@ -0,0 +1,19 @@
+package fr.imie.ProjetPlateformeWeb.dao;
+
+public class StudentDAO {
+	private String em;
+
+	/**
+	 * @return the em
+	 */
+	public String getEm() {
+		return em;
+	}
+
+	/**
+	 * @param em the em to set
+	 */
+	public void setEm(String em) {
+		this.em = em;
+	}
+}
